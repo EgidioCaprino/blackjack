c58898414b79d572db9cd36da1bb9e1a12f525ae
Egidio Caprino <egidio.caprino@gmail.com>
2020-02-21 19:32:58 +0100

    chore: throw an error when the deck is not of 52 cards

--- src/fetchDeck.js
+++ src/fetchDeck.js
@@ -2,5 +2,9 @@ import fetch from 'node-fetch';
 
 export default async () => {
   const response = await fetch('https://teston-backend-case.herokuapp.com/shuffle');
-  return response.json();
+  const deck = await response.json();
+  if (deck.length !== 52) {
+    throw new Error(`Invalid deck received: there are ${deck.length} cards`);
+  }
+  return deck;
 };
